24e479bb640ab483bfd791d9e57a5c9fc15071a7
Chai <developer.chaitra@gmail.com>
2023-02-21 02:40:19 +1300

    Tic Tac toe using tailwind css

---
+++ game-app/src/Game.js
@@ -0,0 +1,14 @@
+//this is my home app - for my neurodiergent brain
+//whenever you start the program run npm run dev
+
+function Game() {
+  return (
+    <div>
+      <h1 className='text-center text-blue-500 font-bold underline text-2xl'>
+        Tic Tac Toe with Tailwind CSS
+      </h1>
+    </div>
+  );
+}
+
+export default Game;
